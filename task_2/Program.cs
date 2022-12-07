f6a2b2bac52646b9227ee990d6d0dc79fb4c2211
jan_ser <prosto.rusakov@yandex.ru>
2022-12-07 14:58:26 +0300

    BigSmallRovno

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Console.Clear ();
+
+Console.WriteLine ("Привет-привет. Введи циферку");
+int FirstNumb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("А теперь вторую");
+int SecondNumb = Convert.ToInt32(Console.ReadLine());
+if (FirstNumb > SecondNumb)
+{
+    Console.WriteLine ("Цифра " + FirstNumb + " больше " + SecondNumb + " Thanks!");
+}  
+else
+{
+    if (FirstNumb == SecondNumb)
+{
+    Console.WriteLine ("Цифра " + SecondNumb + " равна " + FirstNumb + " Thanks!");
+}
+else
+{
+    Console.WriteLine ("Цифра " + SecondNumb + " больше " + FirstNumb + " Thanks!");
+}}   
