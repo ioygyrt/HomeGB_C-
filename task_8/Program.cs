25eaf2bd699c30d2c4480307556e3dacb8261ce1
jan_ser <prosto.rusakov@yandex.ru>
2022-12-07 15:55:29 +0300

    chetnie chisla

---
+++ task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear ();
+
+Console.WriteLine ("эхх давайте мне циферку... Выведем все четные числа до этой циферки так и быть");
+int UserNumb = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+while (index <= UserNumb)
+{
+    Console.WriteLine (index);
+    index = index+2;
+}
+Console.WriteLine ("Это все четные числа до числа --> " + UserNumb);
