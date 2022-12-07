23f331487c988ff565733fdc7c8e3f3d32c54a46
jan_ser <prosto.rusakov@yandex.ru>
2022-12-07 15:17:36 +0300

    Max numb

---
+++ task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear ();
+
+Console.WriteLine ("Привет-привет. Введи циферку");
+int FirstNumb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("А теперь вторую");
+int SecondNumb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("А еще третью");
+int ThriNumb = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (FirstNumb < max);
+{
+    max = FirstNumb;
+}
+if (SecondNumb < max); 
+{
+    max = SecondNumb;
+}
+if (ThriNumb > max); 
+{
+    max = ThriNumb;
+}
+Console.WriteLine ("А вот Ваше максимальное число и оно равно = " + max);
