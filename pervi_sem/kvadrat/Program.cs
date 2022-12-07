ff42275d1662b223fe9dd4c83cb82c5129ee8151
jan_ser <prosto.rusakov@yandex.ru>
2022-12-07 14:10:23 +0300

    Good

--- pervi_sem/kvadrat/Program.cs
+++ pervi_sem/kvadrat/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.WriteLine("Для вычисления квадрата введите Ваше число");
+int N = Convert.ToInt32(Console.ReadLine());
+int kukold = N * N;
+Console.WriteLine("Квадрат вашего числа = " + kukold);
