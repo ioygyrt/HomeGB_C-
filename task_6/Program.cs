a5f8612d0297f896de28874bfb4257f0de6fdc61
jan_ser <prosto.rusakov@yandex.ru>
2022-12-07 15:42:24 +0300

    Chetnoe Chislo

---
+++ task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear ();
+
+Console.WriteLine ("Привет-привет. Введите циферку, а я скажу четная она или нет");
+int UserNumb = Convert.ToInt32(Console.ReadLine());
+if (UserNumb % 2 == 0)
+{ 
+    Console.WriteLine ("Число " + UserNumb + " является четным");
+}
+else 
+{
+    Console.WriteLine ("Число " + UserNumb + " является нечетным");
+}
