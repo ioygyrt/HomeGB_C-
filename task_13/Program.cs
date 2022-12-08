ab5068bf76f0bccfbd968c53c38b8354a96506fc
jan_ser <prosto.rusakov@yandex.ru>
2022-12-08 19:30:40 +0300

    cifra 3

---
+++ task_13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear ();
+Console.WriteLine("Напишите ваши циферки сколько угодно. дальше видно будет ");
+int randomUserNumb = Convert.ToInt32(Console.ReadLine());
+string numberus = Convert.ToString(randomUserNumb);
+if (numberus.Length > 2){
+  Console.WriteLine("третья цифра -> " + numberus[2]);
+}
+else 
+  Console.WriteLine("-> нету третьей цифры-то, нееету =) ");
