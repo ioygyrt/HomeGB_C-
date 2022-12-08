90b235c4af55ff9be2787b5263643395b3400e92
jan_ser <prosto.rusakov@yandex.ru>
2022-12-08 19:21:56 +0300

    secondnumb

---
+++ task_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Снова здравствуйте, введите число из 3 знаков, а я Вам покажу какая цифра у вас вторая =)");
+int NumbUser= Convert.ToInt32(Console.ReadLine());
+string stringSeconNumb = Convert.ToString(NumbUser);
+Console.WriteLine("вторая цифра этого числа -> "+stringSeconNumb[1]);
