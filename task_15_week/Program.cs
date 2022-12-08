31713deea40617718841517caa4b72d6ebb18407
jan_ser <prosto.rusakov@yandex.ru>
2022-12-08 19:39:30 +0300

    week

---
+++ task_15_week/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear ();
+
+Console.WriteLine("Давайте проверим... Введите цифру от 1 до 7 и я скажу Вам выходной это или нет =) ");
+int UserDayWeek = Convert.ToInt32(Console.ReadLine());
+
+void CheckDayWeek (int UserDayWeek) {
+  if (UserDayWeek == 6 || UserDayWeek == 7) {
+  Console.WriteLine("Урррра выходноой");
+  }
+  else if (UserDayWeek < 1 || UserDayWeek > 7) {
+    Console.WriteLine("Ну что вы дней то всего 7");
+  }
+  else Console.WriteLine("Иди работай");
+}
+
+CheckDayWeek(UserDayWeek);
