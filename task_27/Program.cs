e51ddce54d6c0b1e91e46cd041b00192f01c28d5
jan_ser <prosto.rusakov@yandex.ru>
2022-12-12 13:41:43 +0300

    task27

---
+++ task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int num){
+    
+    int vvod = Convert.ToString(num).Length;
+    int dvi = 0;
+    int result = 0;
+
+    for (int i = 0; i < vvod; i++){
+      dvi = num - num % 10;
+      result = result + (num - dvi);
+      num = num / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(num);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
